467d5708805329f3f56646d423bc1df966a9d519
kriem2000 <88950529+kriem2000@users.noreply.github.com>
2022-07-31 23:23:48 +0200

    seeders

---
+++ database/seeders/AirportSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Airport;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AirportSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Airport::factory()->count(10)->create();
+    }
+}
